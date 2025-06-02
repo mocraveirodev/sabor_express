8883c004b4449f46e7633b462376bc007a1f8b4d
Monica Craveiro <craveiromonica@gmail.com>
2025-06-02 07:07:13 -0300

    condicional if e função int para conversão em inteiro

--- app.py
+++ app.py
@@ -12,5 +12,17 @@ print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair \n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+# print(f'Você escolheu a opção {opcao_escolhida}')
+# print(opcao_escolhida ==1) #false
+# print(type(opcao_escolhida)) #str
+# print(type(1)) #int
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
