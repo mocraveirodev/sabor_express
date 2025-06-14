158b6c673cc339fdceb8489e6616e9d9e897953d
Monica Craveiro <craveiromonica@gmail.com>
2025-06-13 22:31:21 -0300

    __main__ e separando funcionalidades em funções

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-print('''
+def exibir_nome_app():
+    print('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -9,27 +10,38 @@ print('''
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
 ''')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair \n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-# print(f'Você escolheu a opção {opcao_escolhida}')
-# print(opcao_escolhida ==1) #false
-# print(type(opcao_escolhida)) #str
-# print(type(1)) #int
+def exibir_menu():
+    print('Menu:')
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair \n')
 
 def finalizar_app():
     os.system('cls' if os.name == 'nt' else 'clear') # Caso esteja no Windows, usa 'cls', caso contrário, usa 'clear'
     print('Obrigado por utilizar nosso app')
     exit()
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+def obter_opcao_usuario():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # print(f'Você escolheu a opção {opcao_escolhida}')
+    # print(opcao_escolhida ==1) #false
+    # print(type(opcao_escolhida)) #str
+    # print(type(1)) #int
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_app()
+    exibir_menu()
+    obter_opcao_usuario()
+
+if __name__ == '__main__':
+    main()
